8fd906f8970ceb9551f936347ff59475c484dfab
Aiden Dewar <aidendewar@Aidens-MacBook-Pro.local>
2025-02-11 20:08:53 +0000

    fixed templates, index page working correctly

--- airtales/urls.py
+++ airtales/urls.py
@@ -17,11 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from airtalesapp import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('airtalesapp/', include('airtalesapp.urls')),
+    path('', include('airtalesapp.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- airtalesapp/views.py
+++ airtalesapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(request, 'airtalesapp/index.html')
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("about")
