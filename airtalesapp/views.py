949aaad2606e6b0ffe5673033d640674f7eea69c
Aiden Dewar <aidendewar@Aidens-MacBook-Pro.local>
2025-02-11 19:53:16 +0000

    index template added, not working yet

--- airtalesapp/views.py
+++ airtalesapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Testing")
+    return HttpResponse(request, 'airtalesapp/index.html')
+
+def about(request):
+    return HttpResponse("about")
 
 
