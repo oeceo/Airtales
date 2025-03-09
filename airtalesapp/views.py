ccc8f345a15c8010bfef1481526cca6be7917066
Chelsea Gordon <2271340g@student.gla.ac.uk>
2025-03-09 12:13:53 +0000

    Made prompt function reusable and added to index
    - Added function in views.py for today's date and yesterday's date.
    - Moved prompt functionality from the profile view function into a reusable function.
    - Added the new date and prompt functions to the profile and index views.
    - Updated styling.

--- airtalesapp/views.py
+++ airtalesapp/views.py
@@ -10,7 +10,27 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 
 def index(request):
-    return render(request, 'index.html')
+    
+    today = todays_date()
+    yesterday = yesterdays_date()
+
+    current_prompt_text = get_prompt(today)
+    yesterdays_prompt_text = get_prompt(yesterday)
+
+    current_post_position1 = 0
+    current_post_position2 = 0
+    current_post_position3 = 0
+
+    yesterday_post_position1 = 0
+    yesterday_post_position2 = 0
+    yesterday_post_position3 = 0
+
+    context = {
+        'current_prompt_text': current_prompt_text,
+        'yesterdays_prompt_text': yesterdays_prompt_text,
+    }
+
+    return render(request, 'index.html', context)
 
 def about(request):
     return render(request, 'about.html')
@@ -46,14 +66,12 @@ def save_entry(request):
 
 @login_required
 def profile(request):
-    #this returns the days prompt to the profile
-    today = now().date()  # Get today's date
-    prompt_text = "No prompt available for today."  # default if there is no prompt
-    try:
-        prompt = Prompt.objects.get(date=today)#get the prompt for the day
-        prompt_text = prompt.prompt  
-    except Prompt.DoesNotExist:
-        pass  
+
+    today = todays_date()
+ 
+    # this returns the prompt to the profile
+    prompt_text = get_prompt(today)
+
     #this checks if the user has already made a journal entry
     prior_entry = JournalEntry.objects.filter(userID=request.user, date=today).exists()
 
@@ -71,5 +89,24 @@ def profile(request):
 def view_entry(request, entry_id):
     entry = get_object_or_404(JournalEntry, id=entry_id)
     return render(request, 'view_entry.html', {'entry': entry})
+
 def userjournal(request):
     return render(request, 'userjournal.html')
+
+def todays_date():
+    date = now().date()
+    return date
+
+def yesterdays_date():
+    date = now().date() - timedelta(days=1)
+    return date
+
+def get_prompt(date):
+    #this returns the prompt of the passed date
+    prompt_text = "No prompt available."  # default if there is no prompt
+    try:
+        prompt = Prompt.objects.get(date=date) #get the prompt for the day
+        prompt_text = prompt.prompt  
+    except Prompt.DoesNotExist:
+        pass
+    return prompt_text
